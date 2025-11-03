5e4bda74c5c29198a57080a34cf2bc0cce810e0f
daahyunk <ekgusdl0408@naver.com>
2025-11-03 17:46:57 +0900

    test(utils): 구입 금액 검증 실패 케이스 추가

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,7 @@
+import { validatePurchaseAmount } from "../utils/validation.js";
+
+describe("구입 금액 입력 검증", () => {
+  test("1000원 단위가 아닐 경우 예외를 던진다", () => {
+    expect(() => validatePurchaseAmount("1500")).toThrow("[ERROR]");
+  });
+});
