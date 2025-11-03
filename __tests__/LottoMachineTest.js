ffee21d5a6210e18477434075dc34fff488e114c
daahyunk <ekgusdl0408@naver.com>
2025-11-03 19:43:10 +0900

    test(domain): 로또 발행 기능 테스트 추가

---
+++ __tests__/LottoMachineTest.js
@@ -0,0 +1,25 @@
+import LottoMachine from "../domain/LottoMachine.js";
+
+describe("LottoMachine", () => {
+  test("구입 금액에 따라 구매 가능한 로또 개수를 계산한다", () => {
+    const lottoMachine = new LottoMachine();
+    const tickets = lottoMachine.issue(8000);
+
+    expect(tickets.length).toBe(8);
+  });
+
+  test("각 로또는 1~45 사이의 중복되지 않은 6개 숫자를 가진다", () => {
+    const lottoMachine = new LottoMachine();
+    const tickets = lottoMachine.issue(1000);
+
+    const numbers = tickets[0].numbers;
+    const uniqueNumbers = new Set(numbers);
+
+    expect(numbers.length).toBe(6);
+    expect(uniqueNumbers.size).toBe(6);
+    numbers.forEach((num) => {
+      expect(num).toBeGreaterThanOrEqual(1);
+      expect(num).toBeLessThanOrEqual(45);
+    });
+  });
+});
