1b17fedb5b9aa88f8d0662d41edee160067f3134
daahyunk <ekgusdl0408@naver.com>
2025-11-03 21:06:54 +0900

    refactor: 보너스 번호 검증 로직 네이밍 및 가독성 개선

--- src/domain/BonusNumber.js
+++ src/domain/BonusNumber.js
@@ -4,23 +4,25 @@ class BonusNumber {
   #number;
 
   constructor(input, winningNumbers) {
-    const number = Number(input);
-    this.#validate(number, winningNumbers);
+    const number = Number.parseInt(input, 10);
+    this.#validateBonusNumber(number, winningNumbers);
     this.#number = number;
   }
 
-  #validate(number, winningNumbers) {
+  #validateBonusNumber(number, winningNumbers) {
     const { INVALID_BONUS_NUMBER } = ERROR_MESSAGE;
 
     if (Number.isNaN(number)) {
       throw new Error(INVALID_BONUS_NUMBER);
     }
 
-    if (number < MIN_NUMBER || number > MAX_NUMBER) {
+    const isOutOfRange = number < MIN_NUMBER || number > MAX_NUMBER;
+    if (isOutOfRange) {
       throw new Error(INVALID_BONUS_NUMBER);
     }
 
-    if (winningNumbers.includes(number)) {
+    const isDuplicated = winningNumbers.includes(number);
+    if (isDuplicated) {
       throw new Error(INVALID_BONUS_NUMBER);
     }
   }
